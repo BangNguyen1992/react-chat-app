51a5f677d4bc5b39680a7e340d2c772c417709ab
BangNguyen1992 <nguyenbahaibang@gmail.com>
2018-10-31 15:23:57 +0200

    build test for heroku

---
+++ client/build/precache-manifest.563ea47f5386787c0076340d67d83c63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a69332e8ebe6b73f9d74",
+    "url": "/static/js/main.a69332e8.chunk.js"
+  },
+  {
+    "revision": "b6ed1a47aaa04af981ba",
+    "url": "/static/js/1.b6ed1a47.chunk.js"
+  },
+  {
+    "revision": "a69332e8ebe6b73f9d74",
+    "url": "/static/css/main.cf684834.chunk.css"
+  },
+  {
+    "revision": "8aa87e54a9893adb4540e0424efd3c97",
+    "url": "/index.html"
+  }
+];
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const Chatkit = require('@pusher/chatkit-server');
+var path = require("path");
 
 const app = express();
 const port = process.env.PORT || 3001;
